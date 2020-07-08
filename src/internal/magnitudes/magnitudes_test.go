9db4000bda9e238b9fe085c056f5d7c709fa6212
rafael-santiago <voidbrainvoid@tutanota.com>
2020-07-08 06:07:20 -0800

    ruler and magnitudes tests

---
+++ src/internal/magnitudes/magnitudes_test.go
@@ -0,0 +1,35 @@
+//                          Copyright (C) 2020 by Rafael Santiago
+//
+// Use of this source code is governed by GPL-v2 license that can
+// be found in the COPYING file.
+package magnitudes
+
+import (
+    "testing"
+)
+
+func TestGetCourier12pxParams(t *testing.T) {
+    charPerLineNr, charPerPageNr := GetCourier12pxParams()
+    if charPerLineNr != charPerLineA4Courier12px {
+        t.Errorf(`charPerLineNr != charPerLineA4Courier12px`, charPerLineNr, charPerLineA4Courier12px)
+    }
+    if charPerPageNr != charPerPageA4Courier12px {
+        t.Errorf(`charPerPageNr != charPerPageA4Courier12px`, charPerPageNr, charPerPageA4Courier12px)
+    }
+}
+
+func TestGetCourier10pxParams(t *testing.T) {
+    charPerLineNr, charPerPageNr := GetCourier10pxParams()
+    if charPerLineNr != charPerLineA4Courier10px {
+        t.Errorf(`charPerLineNr != charPerLineA4Courier10px`, charPerLineNr, charPerLineA4Courier10px)
+    }
+    if charPerPageNr != charPerPageA4Courier10px {
+        t.Errorf(`charPerPageNr != charPerPageA4Courier10px`, charPerPageNr, charPerPageA4Courier10px)
+    }
+}
+
+func TestGetA4PaperWidthSizeInMM(t *testing.T) {
+    if size := GetA4PaperWidthSizeInMM(); size != a4PaperWidthInMM {
+        t.Errorf(`GetA4PaperSizeInMM() != A4PaperWidthInMM`, size, a4PaperWidthInMM)
+    }
+}
---
+++ src/internal/ruler/ruler_test.go
@@ -0,0 +1,34 @@
+//                          Copyright (C) 2020 by Rafael Santiago
+//
+// Use of this source code is governed by GPL-v2 license that can
+// be found in the COPYING file.package ruler
+package ruler
+
+import (
+    "testing"
+    "internal/magnitudes"
+)
+
+func TestCalibrateCourier12px(t *testing.T) {
+    codestat := &CodeStat{}
+    codestat.CalibrateCourier12px()
+    charPerLine, charPerPage := magnitudes.GetCourier12pxParams()
+    if codestat.CharPerLine != charPerLine {
+        t.Errorf(`charPerLine != codestat.CharPerLine`, charPerLine, codestat.CharPerLine)
+    }
+    if codestat.CharPerPage != charPerPage {
+        t.Errorf(`codestat.CharPerPage != charPerPage`, codestat.CharPerPage, charPerPage)
+    }
+}
+
+func TestCalibrateCourier10x(t *testing.T) {
+    codestat := &CodeStat{}
+    codestat.CalibrateCourier10px()
+    charPerLine, charPerPage := magnitudes.GetCourier10pxParams()
+    if codestat.CharPerLine != charPerLine {
+        t.Errorf(`charPerLine != codestat.CharPerLine`, charPerLine, codestat.CharPerLine)
+    }
+    if codestat.CharPerPage != charPerPage {
+        t.Errorf(`codestat.CharPerPage != charPerPage`, codestat.CharPerPage, charPerPage)
+    }
+}
